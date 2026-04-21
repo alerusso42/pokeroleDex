a9eedd066d506fc6d28572c324b55c9321f4fd31
alerusso42 <alerusso@DESKTOP-9FJQO5K.localdomain>
2026-04-21 21:38:30 +0200

    create fatto

--- app/code/trainer.js
+++ app/code/trainer.js
@@ -2,8 +2,10 @@ const lib = require("./utils/lib.js");
 
 /** @typedef {typeof import("../data/questData/template/trainer.json")} Trainer */
 
-const questDataPath = "../data/questData/";
-const trainerDirPath = questDataPath + "trainer/";
+const	questDataPath = "../data/questData/";
+const	trainerDirPath = questDataPath + "trainer/";
+const	trainerTemplate = questDataPath + "template/" + "trainer.json";
+const	pokemonTemplate = questDataPath + "template/" + "pokemon.json";
 
 /**
  * 
@@ -52,9 +54,12 @@ function editTrainer(server, client, res)
 function parseTeam(server, team)
 {
 	let	pkmnNorm;
+	let	i;
 
+	i = -1;
 	for (let pkmn of team)
 	{
+		i++;
 		let	type = pkmn.type;
 		console.log(type);
 		if (type == "DELETE")
@@ -67,6 +72,7 @@ function parseTeam(server, team)
 			if (pkmn.name == null || typeof(pkmn.data) != "object")
 				return (`bad request format`);
 			pkmnNorm = lib.utils.dataNormalize(pkmn.name);
+			team[i].name = pkmnNorm;
 			if (server.data.pokedex.indexOf(pkmnNorm) == -1)
 				return (`${pkmnNorm} specie does not exist`);
 		}
@@ -113,8 +119,84 @@ function deletePkmn(server, trainerNewData, pkmn)
  */
 function createPkmn(server, trainerNewData, pkmn)
 {
-	console.log(pkmn.name, pkmn.data);
+	let	id = createUniqueId(server, pkmn.name);
+	pkmn.id = id;
+	let	specieFilename = server.data.GetPath("pokedex") + pkmn.name + ".json";
+	let	newFilename = server.data.GetPath("pokemon") + id + ".json";
+	let	dataTemplate = JSON.parse(lib.fs.readFileSync(pokemonTemplate));
+	let	dataSpecie = JSON.parse(lib.fs.readFileSync(specieFilename));
+	let	dataNew = dataTemplate;
+
+	// 1. Dati Identità
+    dataNew.Species = dataSpecie.Name;
+    dataNew.Name = dataSpecie.Name;
+    dataNew.Trainer = trainerNewData.Name;
+    dataNew.Type1 = dataSpecie.Type1;
+    dataNew.Type2 = dataSpecie.Type2 || "";
+    dataNew.Id = id;
+
+    // 2. Mappatura Attributi (Current e Max)
+    dataNew.HP.current = dataSpecie.BaseHP;
+    dataNew.HP.max = dataSpecie.BaseHP;
+    const attrs = ["Strength", "Dexterity", "Vitality", "Special", "Insight"];
+    attrs.forEach((attr) => 
+	{
+        if (dataSpecie[attr] !== undefined)
+		{
+            dataNew[attr].current = dataSpecie[attr];
+            dataNew[attr].max = dataSpecie[`Max${attr}`] || dataSpecie[attr];
+        }
+    });
+	const skills = ["Brawl", "Channel", "Clash", "Evasion", "Alert", "Athletics", "Nature", "Stealth", "Initiative"];
+	for (let skill of skills)
+		dataNew[skill] = 1;
+
+    // 3. Abilità
+    if (dataSpecie.Ability1)
+	{
+        dataNew.Ability.Name = dataSpecie.Ability1;
+        dataNew.Ability.Effect = "";
+        dataNew.Ability.Description = "";
+    }
+
+    // 4. Dati Fisici e Bio
+    dataNew.Height = dataSpecie.Height;
+    dataNew.Weight = dataSpecie.Weight;
+    dataNew.DexDescription = dataSpecie.DexDescription;
+    dataNew.Evolutions = dataSpecie.Evolutions;
+    dataNew.Moves = dataSpecie.Moves.map((m) => 
+	{
+		return { Name: m.Name, Learned: m.Learned }; 
+    });
+	dataNew.Moves.slice(0, 4);
+	lib.fs.writeFileSync(newFilename, JSON.stringify(dataNew, null, 2), 'utf-8');
+	server.data.pokemon.push(id);
+	trainerNewData.Pokemon.push(id);
 	return (null);
+}
+
+/**
+ * 
+ * @param {lib.types.Server} server 
+ * @param {String} pkmn 
+ * @returns {string} unique id
+ */
+function	createUniqueId(server, pkmn)
+{
+	let	i;
+	let	path;
+
+	path = pkmn;
+	if (server.data.pokemon.includes(path) == false)
+		return (pkmn);
+	i = 1;
+	path = `${pkmn}_${i}`;
+	while (server.data.pokemon.includes(path) == true)
+	{
+		i++;
+		path = `${pkmn}_${i}`;
+	}
+	return (`${pkmn}_${i}`);
 }
 
 // function	print(data, key="")
