f15ec11c08fb103de3ec693980c3f1965d84e684
alerusso42 <alerusso@DESKTOP-9FJQO5K.localdomain>
2026-04-28 19:26:18 +0200

    controllo trainer donna

--- app/code/autoindex.js
+++ app/code/autoindex.js
@@ -1,7 +1,9 @@
+// @ts-check
 const lib = require('./utils/lib.js');
 const { Server } = require('./utils/types.js');
 
-const trainerUrl = "https://play.pokemonshowdown.com/sprites/trainers/lucas-gen4pt.png";
+const trainerBoyUrl = "https://play.pokemonshowdown.com/sprites/trainers/lucas-gen4pt.png";
+const trainerGirlUrl = "https://play.pokemonshowdown.com/sprites/trainers/dawn-gen4pt.png";
 
 /**
  * @param {lib.types.Server} server
@@ -12,11 +14,17 @@ const trainerUrl = "https://play.pokemonshowdown.com/sprites/trainers/lucas-gen4
 function autoIndex(server, client, res)
 {
 	let list = client.doc.getElementById("autoindex");
-	let url = trainerUrl;
+	let url;
 
+	if (!list)
+		return (res.send(client.dom.serialize()));
 	client.dirName = client.dirName.toLowerCase().replaceAll("/", " ");
 	for (let file of server.data[client.dirName])
 	{
+		url = trainerBoyUrl;
+		if (isFemale(file))
+			url = trainerGirlUrl;
+		console.log(file);
 		file = file.replace(".json", "");
 		list.innerHTML += `
 			<div class="trainer-item">
@@ -26,6 +34,20 @@ function autoIndex(server, client, res)
 		`;
 	}
 	res.send(client.dom.serialize());
+}
+
+/**
+ * @param {String} name
+ */
+function isFemale(name)
+{
+	let	names;
+
+	names = name.split(" ");
+	name = names[0];
+	if (name.at(name.length - 1) == "a")
+		return (true);
+	return (false);
 }
 
 module.exports = {autoIndex};
